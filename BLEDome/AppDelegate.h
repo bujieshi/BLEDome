ad7f72ad0e7cf0dab7999de16cbe9fa75935dac4
zhoujian <zhoujian@localhost>
2015-12-14 11:54:16 +0800

    Initial Commit

---
+++ BLEDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BLEDome
+//
+//  Created by zhoujian on 15/12/14.
+//  Copyright © 2015年 zhoujian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BLEDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BLEDome
+//
+//  Created by zhoujian on 15/12/14.
+//  Copyright © 2015年 zhoujian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
